949d80a78ae95859285211178961dadb33b21956
Sergio de la Garma López <sdlg.oficial@outlook.es>
2025-06-07 12:14:33 +0200

    Update tests_curso.py

--- data/tests_curso.py
+++ data/tests_curso.py
@@ -1,5 +1,5 @@
-def test_calcular_roi():
-    assert abs(calcular_roi(1000, 1200) - 0.2) < 0.0001, "Error en test 1"
-    assert abs(calcular_roi(500, 750) - 0.5) < 0.0001, "Error en test 2"
-    assert abs(calcular_roi(2000, 1800) + 0.1) < 0.0001, "Error en test 3"
+def test_calcular_roi(fun):
+    assert abs(fun(1000, 1200) - 0.2) < 0.0001, "Error en test 1"
+    assert abs(fun(500, 750) - 0.5) < 0.0001, "Error en test 2"
+    assert abs(fun(2000, 1800) + 0.1) < 0.0001, "Error en test 3"
     print("✅ Todos los tests de ROI pasaron.")
